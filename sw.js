231e6222ce9cb7c9a70eda5697b6f6bd6407b5a3
witek746653-maker <witek746653@gmail.com>
2025-08-21 20:16:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,35 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("roma-pwa-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.webmanifest",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ]);
-    })
-  );
+const CACHE = 'roma-pwa-v2'; // повысил версию, чтобы сбросить старый кеш
+const CORE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-180.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(cached => cached ||
+      fetch(e.request).then(r => {
+        const rc = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc)).catch(()=>{});
+        return r;
+      }).catch(() => cached)
+    )
   );
 });
 
