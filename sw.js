9247d47ea87ed1f1cdf58f4775479c1c8a20adc1
witek746653-maker <witek746653@gmail.com>
2025-08-21 19:43:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('roma-pwa-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/manifest.json', '/sun_transparent.png']);
+    caches.open("roma-pwa-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.webmanifest",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+
