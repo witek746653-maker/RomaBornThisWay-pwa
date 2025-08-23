62cf9800f6ae9a445a0436e07e1884107589a7b3
witek746653-maker <witek746653@gmail.com>
2025-08-23 07:16:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+
+// sw.js
+const CACHE = 'roma-pwa-v3'; // bumped to v3 to invalidate old cache
+const CORE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-180.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached ||
+      fetch(e.request).then(r => {
+        const rc = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, rc)).catch(()=>{});
+        return r;
+      }).catch(() => cached)
+    )
+  );
+});
